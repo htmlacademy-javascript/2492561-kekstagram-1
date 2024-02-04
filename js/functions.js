9bcd34db10ca278e84e2d3295483e2ded8eb3375
Toriboba <geor.chudin@gmail.com>
2024-02-04 14:54:42 +0300

    Функции

--- js/functions.js
+++ js/functions.js
@@ -9,6 +9,22 @@ function isPalindrome (string){
   return (string === reverseString);
 }
 
+function isPalindrome2 (string){
+  string = string.toLowerCase();
+  string = string.replaceAll(' ','');
+  let left = 0;
+  let right = string.length - 1;
+  while (left < right) {
+    if (string[left] !== string[right]) {
+      return false;
+    }
+    left++;
+    right--;
+  }
+  return true;
+}
+
+
 //Функция, которая принимает строку, извлекает содержащиеся в ней цифры от 0 до 9 и возвращает их в виде целого положительного числа.
 function getNumber (string) {
   let number = '';
@@ -21,17 +37,26 @@ function getNumber (string) {
 }
 
 //Функция, которая принимает три параметра: исходную строку, минимальную длину и строку с добавочными символами — и возвращает исходную строку, дополненную указанными символами до заданной длины.
-function editingString (string, length, symbol) {
-  let editedString = '';
-  while (editedString.length <= length - string.length) {
-    if (editedString.length === length - string.length) {
+function getEditingString(string, length, symbol) {
+  let result = string;
+  while (result.length < length) {
+    const diff = result.length + symbol.length - length;
+    if (diff > 0) {
+      let slicedSymbol = '';
+      for (let i = 0; i < symbol.length - diff; i++) {
+        slicedSymbol += symbol[i];
+      }
+      result = slicedSymbol + result;
       break;
     }
-    if (editedString.length + symbol.length > length - string.length) {
-
-      break;
-    }
-    editedString += String(symbol);
+    result = symbol + result;
   }
-  return editedString + string;
+  return result;
 }
+
+//Функция для проверки длины строки.
+
+function checkLength (string, length) {
+  return string.length <= length;
+}
+
