6ace7f83c2b851585f14039b4776ccee6485229b
Toriboba <geor.chudin@gmail.com>
2024-03-02 23:25:58 +0300

    module7-task1

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
-import { usersPfotoDescriptions,ACCEPTABLE_NUMBER_OF_ID } from './data-generator';
+import { usersPfotoDescriptions,ACCEPTABLE_NUMBER_OF_ID } from './data-generator.js';
+import {pictureRender} from './picture-render.js';
 
-export const photosList = Array.from({length: ACCEPTABLE_NUMBER_OF_ID}, usersPfotoDescriptions);
+const photosList = Array.from({length: ACCEPTABLE_NUMBER_OF_ID}, usersPfotoDescriptions);
+
+const pictures = document.querySelector('.pictures');
+photosList.forEach((photo) => {
+  pictures.appendChild(pictureRender(photo.url, photo.comments.length, photo.likes));
+});
--- js/utilities.js
+++ js/utilities.js
@@ -1,8 +1,34 @@
-const getRandomInteger = (a, b) => {
+export const getRandomInteger = (a, b) => {
   const lower = Math.ceil(Math.min(a, b));
   const upper = Math.floor(Math.max(a, b));
   const result = Math.random() * (upper - lower + 1) + lower;
   return Math.floor(result);
 };
 
-export {getRandomInteger};
+const makeID = () => {
+  let latestID = 0;
+  return {
+    getNewID() {
+      latestID += 1;
+      return latestID;
+    }
+  };
+};
+export const userPhotoMakeID = makeID();
+
+const createUnicCommentID = (min, max) => {
+  const comments = [];
+  return {
+    getUnicID() {
+      let currentID = getRandomInteger(min, max);
+      while (comments.includes(currentID)) {
+        currentID = getRandomInteger(min, max);
+      }
+      comments.push(currentID);
+      return currentID;
+    }
+  };
+};
+export const createdCommentID = createUnicCommentID(1, 1000);
+
+
