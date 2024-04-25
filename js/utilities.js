fd85fb782a339c001414fedb2e27449b7839f976
Toriboba <geor.chudin@gmail.com>
2024-04-25 06:41:05 +0300

    module11-task1

--- js/utilities.js
+++ js/utilities.js
@@ -1,34 +1,67 @@
-export const getRandomInteger = (a, b) => {
-  const lower = Math.ceil(Math.min(a, b));
-  const upper = Math.floor(Math.max(a, b));
-  const result = Math.random() * (upper - lower + 1) + lower;
-  return Math.floor(result);
+//Ошибка при загрузке данных с сервера
+export const getloadDataAlert = (message) => {
+  const alertContainer = document.createElement('div');
+  alertContainer.style.zIndex = '100';
+  alertContainer.style.width = '700px';
+  alertContainer.style.margin = 'auto';
+  alertContainer.style.padding = '10px 3px';
+  alertContainer.style.fontSize = '30px';
+  alertContainer.style.textAlign = 'center';
+  alertContainer.style.backgroundColor = 'transparent';
+  alertContainer.style.lineHeight = '1';
+
+  alertContainer.textContent = message;
+
+  document.body.append(alertContainer);
 };
 
-const makeID = () => {
-  let latestID = 0;
-  return {
-    getNewID() {
-      latestID += 1;
-      return latestID;
+
+//Обработчик кнопок на оверлее сообщений отправки данных
+
+export const resultOfSend = {
+  Success: 'success',
+  Error: 'error'
+};
+
+const eventButtonInSendOverlay = (succesOrError,) => {
+  const okButton = document.querySelector(`.${succesOrError}__button`);
+  const removedElement = document.querySelector(`section.${succesOrError}`);
+  const alertField = removedElement.querySelector(`.${succesOrError}__inner`);
+  okButton.addEventListener('click', () => {
+    removedElement.remove();
+  });
+  removedElement.addEventListener('keydown', (evt) => {
+    if (evt.key === 'Escape') {
+      evt.preventDefault();
+      removedElement.remove();
+      evt.stopPropagation();
     }
-  };
-};
-export const userPhotoMakeID = makeID();
-
-const createUnicCommentID = (min, max) => {
-  const comments = [];
-  return {
-    getUnicID() {
-      let currentID = getRandomInteger(min, max);
-      while (comments.includes(currentID)) {
-        currentID = getRandomInteger(min, max);
-      }
-      comments.push(currentID);
-      return currentID;
+  });
+  removedElement.addEventListener('click', (evt) => {
+    if(!alertField.contains(evt.target)) {
+      removedElement.remove();
     }
-  };
+  });
 };
-export const createdCommentID = createUnicCommentID(1, 1000);
 
+//Уведомление при отправке данных формы
+export const sendDataResult = (succesOrError) => {
+  const sendErrorOverlay = document.createDocumentFragment();
+  const sendErrorMessage = document.querySelector(`#${succesOrError}`).content.cloneNode(true);
+  sendErrorOverlay.appendChild(sendErrorMessage);
+  document.body.appendChild(sendErrorOverlay);
+  eventButtonInSendOverlay(succesOrError);
+};
+
+//Экран загрузки
+export const showDownloadOverlay = () => {
+  const downloadOverlay = document.createDocumentFragment();
+  const downloadOverlayTemplate = document.querySelector('#messages').content.cloneNode(true);
+  downloadOverlay.appendChild(downloadOverlayTemplate);
+  document.body.appendChild(downloadOverlay);
+};
+
+export const hideDownloadOverlay = () => {
+  document.querySelector('.img-upload__message').remove();
+};
 
